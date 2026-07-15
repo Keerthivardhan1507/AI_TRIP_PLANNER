777e4c47ab7c20edba85bd8b8cf5bb60b1b0af6a
Keerthivardhan1507 <nettemKeerthivardhannaidu1507@gmail.com>
2026-07-15 13:56:20 +0530

    Place search utils

--- utils/place_info_search.py
+++ utils/place_info_search.py
@@ -11,7 +11,57 @@ class GooglePlaceSearchTool:
         
     def google_search_attractions(self,place:str)->dict:
         """search for the attractive places near the place using googleplaces api"""
-        self.places_tool.run(f"top attractive places in and around{place}")
+        return self.places_tool.run(f"top attractive places in and around{place}")
+        
+    def google_search_restaurents(self,place:str)->dict:
+        """search for the nearby restautrents from the place using google places api"""
+        return self.places_tool.run(f"top restaurents in {place}")
+    
+    def google_search_activity(self,place:str)->dict:
+        """ Search for special activity in the search place"""
+        return self.places_tool.run(f"Activities in and around{place}")
+    def google_search_transportation(self,place:str)->dict:
+        """search for the transportation in the place"""
+        return self.places_tool.run(f"Available transportation in {place}")
+    
+class TavilyPlaceSearchTool:
+    def __init__(self):
+        pass
+    
+    def tavily_search_attractions(self,place:str)->dict:
+        """search for the nearby attractive places using TavilySearch"""
+        tavily_tool = TavilySearch(topic = "general",include_answer = "advanced")
+        result = tavily_tool.invoke({"query":f"top neraby attractive places in {place}"})
+        if isinstance(result,dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+    def tavily_search_restaurents(self,place:str)->dict:
+        """search for the nearby restaurents in the place using TavilySearch"""
+        tavily_tool = TavilySearch(topic = "general",include_answer = "advanced")
+        result = tavily_tool.invoke({"query":f"top restaurents in the neraby {place}"})
+        if isinstance(result,dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+    def tavily_search_activity(self,place:str)->dict:
+        """search for any activity going on in the place using TavilySearch"""
+        tavily_tool = TavilySearch(topic = "general",include_answer = "advanced")
+        result = tavily_tool.invoke({"query":f"Activities going on {place}"})
+        if isinstance(result,dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+    def tavily_search_transportation(self,place:str)->dict:
+        """search for the transportation in the place using TavilySearch"""
+        tavily_tool = TavilySearch(topic = "general",include_answer = "advanced")
+        result = tavily_tool.invoke({"query":f"available transprtation in {place}"})
+        if isinstance(result,dict) and result.get("answer"):
+            return result["answer"]
+        return result
+    
+        
+        
         
     
         
