d3a790a63f9bfa7d4a60e41a970b01ffe89f6683
Keerthivardhan1507 <nettemKeerthivardhannaidu1507@gmail.com>
2026-07-15 11:46:21 +0530

    utils and tools

--- tools/currency_conversion_tool.py
+++ tools/currency_conversion_tool.py
@@ -1,11 +1,26 @@
 import os
+from typing import List,Dict,Any
+from utils.currency_converter import CurrencyConverter
+from dotenv import load_dotenv
+from  langchain.tools import tool
 
 
 class CurrencyConTool:
     def __init__(self):
-        pass
-    
-    def _setup_tools(self):
-        pass
+        load_dotenv()
+        self.api_key = os.environ.get("EXCHANGE_RATE_API_KEY")
+        self.currency_service = CurrencyConverter(self.api_key)
+        self.currency_converter_tools_list = self._setup_tools()
+        
+    def _setup_tools(self)->List:
+        """setup tools for the cuurency converter tool"""
+        
+        @tool
+        def convert_currency(amount:float,from_curr:str,to_curr:str):
+            """convert currency from one currency to another"""
+            return self.currency_service.convert(amount,from_curr,to_curr)
+        return [convert_currency]
+            
+            
     
     
--- utils/place_info_search.py
+++ utils/place_info_search.py
@@ -1,3 +1,17 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import json
+from langchain_tavily import TavilySearch
+from langchain_google_community import GooglePlacesTool,GooglePlacesAPIWrapper
+
+class GooglePlaceSearchTool:
+    def __init__(self,api_key:str):
+        self.places_wrapper = GooglePlacesAPIWrapper(gplaces_api_key=api_key)
+        self.places_tool = GooglePlacesTool(api_wrapper=self.places_wrapper)
+        
+        
+    def google_search_attractions(self,place:str)->dict:
+        """search for the attractive places near the place using googleplaces api"""
+        self.places_tool.run(f"top attractive places in and around{place}")
+        
+    
+        
